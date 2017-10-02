740e4a8c867e89ff2093622aedd4270d86965605
SpectralVulpine <spectralvulpine@outlook.com>
2017-10-01 18:43:24 -0600

    Add many new effects

--- src/com/SpectraVulpine/walkingwasteland/listeners/PlayerMoveListener.java
+++ src/com/SpectraVulpine/walkingwasteland/listeners/PlayerMoveListener.java
@@ -1,5 +1,7 @@
 package com.SpectraVulpine.walkingwasteland.listeners;
 
+import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -18,23 +20,63 @@ public class PlayerMoveListener implements Listener {
 		plugin = ww;
 	}
 
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public static void onPlayerMove(PlayerMoveEvent e) {
+		// TODO: Make nearby mobs take damage
 		if (WastelandManager.isWastelander(e.getPlayer())) {
+			Random r = new Random();
 			Location center = e.getPlayer().getLocation();
-			Location bottomCorner = center.subtract(3, 3, 3);
-			for (int x = 0; x < 7; x++) {
-				for (int y = 0; y < 7; y++) {
-					for (int z = 0; z < 7; z++) {
-						// Iterates through all blocks in a 7x7x7 area, one row at a time
-						Block b = bottomCorner.clone().add(x, y, z).getBlock();
-						if (b.getType() != Material.AIR) {
-							System.out.println("[WASTELAND DEBUG] Got block of type " + b.getType().toString());
-						}
-						if (b.getType() == Material.GRASS) {
-							b.setType(Material.DIRT);
-						} else if (b.getType() == Material.WATER || b.getType() == Material.STATIONARY_WATER) {
-							b.setType(Material.FROSTED_ICE); // frosted ice is like normal ice, but it melts a lot faster
+			Location bottomCorner = center.subtract(1, 1, 1);
+			for (int x = 0; x < 3; x++) {
+				for (int y = 0; y < 4; y++) { // y is higher because the player is 2 blocks tall
+					for (int z = 0; z < 3; z++) {
+						// Iterates through all blocks in a 3x4x3 area, one row at a time
+						if (r.nextInt(100) < 20) { // TODO: Make this configurable
+							Block b = bottomCorner.clone().add(x, y, z).getBlock();
+							if (b.getType() == Material.GRASS) {
+								b.setType(Material.DIRT);
+							} else if (b.getType() == Material.WATER || b.getType() == Material.STATIONARY_WATER) {
+								b.setType(Material.FROSTED_ICE); // frosted ice is like normal ice, but it melts a lot faster
+							} else if (b.getType() == Material.LONG_GRASS || 
+									b.getType() == Material.RED_ROSE || 
+									b.getType() == Material.YELLOW_FLOWER || 
+									b.getType() == Material.DOUBLE_PLANT) {
+								if (r.nextInt(2) < 1) {
+									b.breakNaturally();
+								} else {
+									b.setType(Material.DEAD_BUSH);
+								}
+							} else if (b.getType() == Material.LEAVES || b.getType() == Material.LEAVES_2) {
+								b.breakNaturally();
+							} else if (b.getType() == Material.IRON_ORE || 
+									b.getType() == Material.LAPIS_ORE || 
+									b.getType() == Material.GOLD_ORE || 
+									b.getType() == Material.REDSTONE_ORE || 
+									b.getType() == Material.GLOWING_REDSTONE_ORE || 
+									b.getType() == Material.EMERALD_ORE) {
+								b.setType(Material.COAL_ORE);
+							} else if (b.getType() == Material.QUARTZ_ORE) {
+								b.setType(Material.NETHERRACK);
+							} else if (b.getType() == Material.SOUL_SAND) {
+								b.setType(Material.GRAVEL);
+							} else if (b.getType() == Material.SOIL) {
+								b.setType(Material.DIRT);
+								b.setData((byte) 1);
+							} else if (b.getType() == Material.VINE) {
+								b.breakNaturally();
+							} else if (b.getType() == Material.SUGAR_CANE_BLOCK) {
+								b.breakNaturally();
+							} else if (b.getType() == Material.MOSSY_COBBLESTONE) {
+								b.setType(Material.COBBLESTONE);
+							} else if (b.getType() == Material.BROWN_MUSHROOM || 
+									b.getType() == Material.RED_MUSHROOM || 
+									b.getType() == Material.HUGE_MUSHROOM_1 || 
+									b.getType() == Material.HUGE_MUSHROOM_2) {
+								b.breakNaturally();
+							} else if (b.getType() == Material.SMOOTH_BRICK) {
+								b.setData((byte) 2);
+							}
 						}
 					}
 				}
